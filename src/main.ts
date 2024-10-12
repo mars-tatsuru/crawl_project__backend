b9c57bd0e14d278b7605eddac3262d5662bdadd2
tatsuru koike <t-koike@marsflag.com>
2024-10-12 16:52:55 +0900

    comment out waitState

--- src/main.ts
+++ src/main.ts
@@ -71,7 +71,7 @@ const mainCrawl = async (userId: string, siteUrl: string) => {
       const thumbnailPath = path.join(thumbnailFolder, thumbnailName);
 
       // TODO:Check if the file already exists but this is cause of the time out error
-      await page.waitForLoadState("networkidle");
+      // await page.waitForLoadState("networkidle");
 
       // take a screenshot of the page
       const image = await page.screenshot({ path: thumbnailPath });
